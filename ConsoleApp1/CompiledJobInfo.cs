9100d4ebf7ac36f1b32e42f274c04905e153595d
ClassicRagu <megameowthethird@gmail.com>
2025-12-05 23:53:47 -0600

    Initial commit, add phantom job script

---
+++ ConsoleApp1/CompiledJobInfo.cs
@@ -0,0 +1,29 @@
+﻿namespace ConsoleApp1
+{
+	public class CompiledJobInfo
+	{
+		public string PJob { get; set; }
+        public string PJobDescription {get; set;}
+        public uint MaxLevel {get; set;}
+        public List<int> EXPValues {get; set;}
+        public int TotalEXP {get
+            {
+                return EXPValues.Sum();
+            }
+        }
+        public List<LevelUnlock>[] LevelUnlocks {get; set;}
+
+	}
+
+    public class LevelUnlock
+    {
+        public uint Level {get; set;}
+        public string UnlockType {get; set;}
+        public uint ActionTraitRowID {get; set;}
+        public string ActionTraitName {get; set;}
+        public string ActionTraitEffect {get; set;}
+        public uint ActionTraitIconID {get; set;}
+        public string ActionType {get; set;}
+        public sbyte ActionRange {get; set;}
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,84 @@
+﻿using ConsoleApp1;
+using Lumina.Excel.Sheets.Experimental;
+using Newtonsoft.Json;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        if (args.Length < 1)
+        {
+            Console.WriteLine("Please include a file path");
+            return;
+        }
+
+        string xivPath = args[0];
+
+        var lumina = new Lumina.GameData(xivPath, new() { DefaultExcelLanguage = Lumina.Data.Language.English });
+        #pragma warning disable PendingExcelSchema // Non-experimental doesn't have defined offsets
+        var mkdSupportJobs = lumina.GetExcelSheet<MKDSupportJob>();
+        var mkdTraits = lumina.GetExcelSheet<MKDTrait>();
+        var mkdGrowDataSJob = lumina.GetSubrowExcelSheet<MKDGrowDataSJob>();
+        var actionTransient = lumina.GetExcelSheet<ActionTransient>();
+        List<CompiledJobInfo> compiledJobInfos = new List<CompiledJobInfo>();
+
+        foreach (MKDSupportJob mkdSupportJob in mkdSupportJobs)
+        {
+            CompiledJobInfo compiledJobInfo = new CompiledJobInfo();
+            compiledJobInfo.PJob = mkdSupportJob.Name.ExtractText();
+            compiledJobInfo.PJobDescription = mkdSupportJob.Description.ExtractText();
+            compiledJobInfo.MaxLevel = mkdSupportJob.LevelMax;
+            // surely casting a uint to an int wouldn't cause issues :)
+            List<int> expData = mkdGrowDataSJob[mkdSupportJob.RowId].Select(z => (int)z.Unknown0).ToList();
+            compiledJobInfo.EXPValues = expData;
+            // Freelancer's Level Max is 0, since we don't know how many actions it will get we just make it have 6 entries
+            List<LevelUnlock>[] levelUnlocks = new List<LevelUnlock>[mkdSupportJob.LevelMax != 0 ? mkdSupportJob.LevelMax : 6];
+            uint x = 0;
+            while (x < levelUnlocks.Length)
+            {
+                levelUnlocks[x] = new List<LevelUnlock>();
+                x++;
+            }
+            foreach (MKDSupportJob.ActionsStruct action in mkdSupportJob.Actions)
+            {
+                if (action.LevelUnlock != 0)
+                {
+                    LevelUnlock levelUnlock = new LevelUnlock();
+                    levelUnlock.Level = action.LevelUnlock;
+                    levelUnlock.UnlockType = "Action";
+                    levelUnlock.ActionTraitRowID = action.Action.RowId;
+                    levelUnlock.ActionTraitName = action.Action.Value.Name.ExtractText();
+                    levelUnlock.ActionTraitEffect = actionTransient.GetRow(action.Action.RowId).Description.ExtractText();
+                    levelUnlock.ActionType = action.Action.Value.ActionCategory.Value.Name.ExtractText();
+                    levelUnlock.ActionTraitIconID = action.Action.Value.Icon;
+                    levelUnlock.ActionRange = action.Action.Value.Range;
+                    levelUnlocks[mkdSupportJob.RowId != 0 ? action.LevelUnlock - 1 : action.LevelUnlock / 5 - 1].Add(levelUnlock);
+                }
+            }
+            compiledJobInfo.LevelUnlocks = levelUnlocks;
+            compiledJobInfos.Add(compiledJobInfo);
+        }
+        foreach (MKDTrait mkdTrait in mkdTraits)
+        {
+            if (mkdTrait.Unknown4 > 0)
+            {
+                LevelUnlock levelUnlock = new LevelUnlock();
+                levelUnlock.Level = mkdTrait.Unknown4;
+                levelUnlock.UnlockType = "Trait";
+                levelUnlock.ActionTraitRowID = mkdTrait.RowId;
+                levelUnlock.ActionTraitName = mkdTrait.Unknown0.ExtractText();
+                levelUnlock.ActionTraitEffect = mkdTrait.Unknown1.ExtractText();
+                levelUnlock.ActionTraitIconID = (uint)mkdTrait.Unknown2;
+                compiledJobInfos[mkdTrait.Unknown3].LevelUnlocks[mkdTrait.Unknown4 - 1].Add(levelUnlock);
+            }
+        }
+        // Console.WriteLine(JsonConvert.SerializeObject(compiledJobInfos, Formatting.Indented));
+#pragma warning restore PendingExcelSchema
+        string directoryPath = Path.Combine(Directory.GetCurrentDirectory(), $"json/");
+        if (!Directory.Exists(directoryPath))
+        {
+            Directory.CreateDirectory(directoryPath);
+        }
+        File.WriteAllText(Path.Combine(directoryPath, "PhantomJobs.json"), JsonConvert.SerializeObject(compiledJobInfos, Formatting.Indented));
+    }
+}
